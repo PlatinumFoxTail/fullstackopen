1c35a9ab3d54a729883b94d9e0a2ed1a0a655bc8
Martin Häggblom <martin.haggblom@gmail.com>
2024-09-14 07:02:16 +0300

    actual implementation of 1.6: unicafe step 1

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,14 +1,55 @@
 import { useState } from 'react'
 
+const Header = (props) => {
+  //console.log(props)
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
 const App = () => {
+  const welcome = 'give feedback'
+  const statistics = 'statistics'
+  
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const increaseGood = () => {
+    setGood(good + 1)
+  }
+
+  const increaseNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const increaseBad = () => {
+    setBad(bad + 1)
+  }
+
   return (
     <div>
-      code here
+      <Header course={welcome} />
+
+      <Button onClick={increaseGood} text="good" />
+      <Button onClick={increaseNeutral} text="neutral" />
+      <Button onClick={increaseBad} text="bad" />
+
+      <Header course={statistics} />
+      
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+
     </div>
   )
 }
