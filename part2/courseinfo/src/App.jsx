c9742818b9add3538bddc658980729af58309673
Martin Häggblom <martin.haggblom@gmail.com>
2024-09-20 10:01:16 +0300

    submit 2.5: Separate module step 10

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,31 +1,5 @@
-const Header = ({ course }) => <h2>{course}</h2>
-
-const Total = ({ parts }) =>
-  <>
-    {/* parts = object array, reduce=iterate function, sum=cumulative addition, part=item to be passed to reduce function, 0=initial value */}
-    <b>total of {parts.reduce((sum, part) => sum + part.exercises, 0)} exercises</b>
-  </>
-
-const Part = ({ part }) => 
-  <p>
-    {part.name} {part.exercises}
-  </p>
-
-const Content = ({ parts }) => 
-  <>
-    {/* parts = object array, map=iterate function, key=attribute in React to identify items, part=item to be passed to Part component */}
-    {parts.map(part => <Part key={part.id} part={part} />)}
-  </>
-
-const Course = ({ course }) => {
-  return (
-    <>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </>
-  )
-}
+import React from 'react'
+import Course from './components/course'
 
 const Courses = ({ courses }) => {
   return (
